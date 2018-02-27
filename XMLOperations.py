5dac2c583b11c92b882d2e70eba4f9947116019a
sankar-jayaraman <gsankar.jayaraman@gmail.com>
2018-02-27 01:08:40 +0100

    Added more error checking
    Added more error checking

--- XMLOperations.py
+++ XMLOperations.py
@@ -7,8 +7,21 @@ import xml.etree.ElementTree as ET
 from operationTypeSelector import OperationTypeSelector
 from operationCheckSelector import OperationCheckSelector
 from integerResultWriter import IntegerResultWriter
+from expressionsChecker import ExpressionsChecker
 import xml.dom.minidom as miniD
 import collections
+
+def WriteToOutputXML (results, file, outputDir):
+        writer = IntegerResultWriter()
+        resultXMLString = ET.tostring(writer.Write(results))
+        resultXML = miniD.parseString(resultXMLString)
+        prettyResult = resultXML.toprettyxml()
+        inputFileNameWithoutExt = os.path.splitext(file)[0]
+        outputFileName = os.path.join(outputDir, inputFileNameWithoutExt + '_result.xml') 
+        output = open(outputFileName,'wt')
+        output.write(prettyResult)
+
+
 
 # Setting up command line parser
 parser = argparse.ArgumentParser(description = 'Perform Arithmetic operations from input XML operations files')
@@ -29,34 +42,31 @@ for file in os.listdir(inputDir):
         tree = ET.parse(file)
         root = tree.getroot()
         results = collections.OrderedDict()
-        for child in root:
-            # Selects between simple and complex operations
-            selector = OperationTypeSelector()
-            evaluator = selector.Select(child)
+        expressionsChecker = ExpressionsChecker()
+        try:
+            if(expressionsChecker.IsValidOperationXML(root)):
 
-            # Selects the type of operation to be checked for valid XML
+               for child in root:
+                # Selects between simple and complex operations
+                selector = OperationTypeSelector()
+                evaluator = selector.Select(child)
 
-            operationXMLcheckerSelector = OperationCheckSelector()
-            operationXMLChecker = operationXMLcheckerSelector.Select(child)
-            try:
-                if(operationXMLChecker.IsValidOperationXML(child)):
-                   results[child.attrib['id']] = evaluator.Evaluate(child)
-            except Exception as err:
-                   results[child.attrib['id']] = 'ERROR: ' + err.args[0]
-                   break; 
-        # Writing results to result file
-        writer = IntegerResultWriter()
-        resultXMLString = ET.tostring(writer.Write(results))
-        resultXML = miniD.parseString(resultXMLString)
-        prettyResult = resultXML.toprettyxml()
-        inputFileNameWithoutExt = os.path.splitext(file)[0]
-        outputFileName = os.path.join(outputDir, inputFileNameWithoutExt + '_result.xml') 
-        output = open(outputFileName,'wt')
-        output.write(prettyResult)
+                # Selects the type of operation to be checked for valid XML
+
+                operationXMLcheckerSelector = OperationCheckSelector()
+                operationXMLChecker = operationXMLcheckerSelector.Select(child)
+                try:
+                    if(operationXMLChecker.IsValidOperationXML(child)):
+                       results[child.attrib['id']] = evaluator.Evaluate(child)
+                except Exception as err:
+                       results[child.attrib['id']] = 'ERROR: ' + err.args[0]
+                       break; 
+            WriteToOutputXML(results, file, outputDir)    
+        except SyntaxError as err:
+            results['ERROR: '] = err.args[0]
+            WriteToOutputXML(results, file, outputDir)
     
 print ('All input XML files processed successfully !! ')
-
-
 
 
 
